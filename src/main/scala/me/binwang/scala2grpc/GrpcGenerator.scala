9f676d3fa7335c81edd100f8297e34d8e7787d72
Bin Wang <bin.wang@mail.binwang.me>
2021-02-07 10:27:00 -0500

    add more file path configurations

--- src/main/scala/me/binwang/scala2grpc/GrpcGenerator.scala
+++ src/main/scala/me/binwang/scala2grpc/GrpcGenerator.scala
@@ -2,10 +2,11 @@ package me.binwang.scala2grpc
 
 import java.io.File
 
-class GrpcGenerator(sources: Seq[File], targetDirectory: File) {
+class GrpcGenerator(modelSources: Seq[File], serviceSources: Seq[File], targetDirectory: File) {
 
   def generate(): Unit = {
-    println(sources)
+    println(modelSources)
+    println(serviceSources)
     println(targetDirectory)
   }
 
--- src/main/scala/me/binwang/scala2grpc/Scala2GrpcPlugin.scala
+++ src/main/scala/me/binwang/scala2grpc/Scala2GrpcPlugin.scala
@@ -1,22 +1,23 @@
 package me.binwang.scala2grpc
 
-import sbt.Keys.{resourceManaged, unmanagedSources}
 import sbt._
 
 object Scala2GrpcPlugin extends AutoPlugin {
 
   object autoImport {
-    val sources = taskKey[Seq[File]]("sources")
-    val target = settingKey[File]("target")
+    val grpcScalaModelSources = taskKey[Seq[File]]("sources for Scala case classes")
+    val grpcScalaServiceSources = taskKey[Seq[File]]("sources for Scala services")
+    val grpcFileTarget = settingKey[File]("target directory for GRPC protobuf files")
     val generate = TaskKey[Unit]("scala2grpc", "generate grpc files")
   }
 
   import autoImport._
 
-  val generateTask = Def.task { new GrpcGenerator(sources.value, target.value).generate() }
+  val generateTask = Def.task {
+    new GrpcGenerator(grpcScalaModelSources.value, grpcScalaServiceSources.value, grpcFileTarget.value).generate()
+  }
 
   override lazy val projectSettings = Seq(
-    sources := (unmanagedSources in Compile).value,
     generate := generateTask.value,
   )
 
